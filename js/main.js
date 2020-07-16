7692df14daea17451b28df8bd65ac4ea24bdd9ef
s0369852 <s0369852@gmail.com>
2020-07-17 00:42:07 +0900

    ver3

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    // $(".Hbtn").click(function () {
+    //     $(".navbar_gnb").css({ display: "block" });
+    //     $(".navbar_logbox").css({ display: "block" });
+    // });
+
+    $(".slideimg").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        fade: true,
+        cssEase: "linear",
+        prevArrow: "<button class='prevBtn btns'><i class='xi-arrow-left'></i></button>",
+        nextArrow: "<button class='nextBtn btns'><i class='xi-arrow-right'></i></button>",
+    });
+});
