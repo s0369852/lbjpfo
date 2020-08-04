3561ab7ba2babab8c888c0b98f50f6d5f0ed9541
s0369852 <s0369852@gmail.com>
2020-08-04 16:48:19 +0900

    jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function (){
     $("#Hbtn").click(function(){
         $("#Hbtn").toggleClass("act");
         $(".navbar_gnb").toggleClass("pull");
@@ -10,8 +10,43 @@ $(function () {
         infinite: true,
         speed: 300,
         fade: true,
-        cssEase: "linear",
-        prevArrow: "<button class='prevBtn btns'><i class='xi-arrow-left'></i></button>",
-        nextArrow: "<button class='nextBtn btns'><i class='xi-arrow-right'></i></button>",
+        cssEase: "linear"
     });
+
+    // $("#Product").slick({
+    //     slide: ".pdt",
+    //     slideToShow: 3,
+    //     slidesToScroll : 1,
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 300,
+    //     fade: true,
+    //     cssEase: "linear",
+    //     resposive:[{
+    //         breakpoint: 460,
+    //         settings: {slideToShow: 1}
+    //     }] 
+    // })
+
+    function mobileSize(){
+        var deviceWidth = $(window).width();
+        if (deviceWidth <= 460) {
+            $(".pdt_slide").slick({
+                slide: "section.pdt",
+                slideToShow: 1,
+                slidesToScroll : 1,
+                infinite: true,
+                speed: 300,
+                fade: true,
+                cssEase: "linear"
+            });
+        }
+    };
+
+    var timer = null;
+    $(window).on("resize",function(){
+        clearTimeout(timer)
+        timer = setTimeout(mobileSize,300)
+    })
+    
 });
